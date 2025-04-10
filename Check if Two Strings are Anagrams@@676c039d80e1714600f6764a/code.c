23f5a2fda938d0465f657e71eb5035b23e282dd7
ANURAAG0104 <anuraag0104.becse24@chitkara.edu.in>
2025-04-10 15:19:57 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,20 +1,26 @@
-#include <stdio.h>
-
+#include <stdio.h> 
+#include <string.h> 
+#include <ctype.h> 
+#define MAX_CHAR 256 
+int areAnagrams(char str1[], char str2[]) { 
+    int count1[MAX_CHAR] = {0}; 
+    int count2[MAX_CHAR] = {0};  
+    if (strlen(str1) != strlen(str2)) { 
+        return 0; } 
+        for (int i = 0; str1[i] != '\0'; i++) { 
+            count1[tolower(str1[i])]++; 
+            count2[tolower(str2[i])]++; } 
+            for (int i = 0; i < MAX_CHAR; i++) { 
+                if (count1[i] != count2[i]) { 
+                    return 0; } } return 1; } 
 int main() {
-    char str[100];
-    char to_replace, replace_with;
-
-    fgets(str, sizeof(str), stdin);
-    scanf("%c", &to_replace);
-    getchar();
-    scanf("%c", &replace_with);
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == to_replace) {
-            str[i] = replace_with;
-        }
-    }
-
-    printf("%s", str);
-    return 0;
-}
+    char str1[100], str2[100]; 
+    fgets(str1, sizeof(str1), stdin); 
+    fgets(str2, sizeof(str2), stdin); 
+    str1[strcspn(str1, "\n")] = '\0'; 
+    str2[strcspn(str2, "\n")] = '\0'; 
+    if (areAnagrams(str1, str2)) { 
+        printf("Yes\n"); } 
+    else { 
+        printf("No\n"); } 
+        return 0; }
